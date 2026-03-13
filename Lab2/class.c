b20ff5371fafe125d255b6b5427e17b4ac7d8b2f
Jeongnanum23 <esthernanumr12@gmail.com>
2026-03-13 23:03:28 +0900

    edit addnewclass

--- Lab2/class.c
+++ Lab2/class.c
@@ -163,12 +163,15 @@ int applyMyClasses(int my[], int msize, struct st_class* c[], int csize){
         if(code_check==0){
             printf("No such code of class.\n");
         }
-
-        printf(">> Add more?( 1:Yes 2:No) > ");
-        scanf("%d", &flag);
-        if(flag == 2){
-            break;
+        else{
+            printf(">> Add more?( 1:Yes 2:No) > ");
+            scanf("%d", &flag);
+            if(flag == 2){
+                break;
+            }
         }
+
+
     }
 	
 	return msize;
